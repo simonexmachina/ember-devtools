cff1a5bdc9dc167b7cd379d43d1aec99b0a25897
Simon Wade <simon.wade@gmail.com>
2013-10-18 12:02:07 +1100

    Initial commit

---
+++ ember-debug.js
@@ -0,0 +1,42 @@
+"use strict";
+
+Ember.onLoad('Ember.Application', function(Application) {
+  return Application.initializer({
+    name: 'debug',
+    initialize: function(container, app) {
+      app.debug = {
+        controller: function(name){
+          return container.lookup("controller:" + name);
+        },
+        route: function(name){
+          return container.lookup("route:" + name);
+        },
+        view: function(domElem){
+          return Em.View.views[domElem];
+        },
+        log: function(valueOrPromise) {
+          if( valueOrPromise.then ) {
+            valueOrPromise.then(function(value) {
+              console.log(value);
+            });
+          }
+          else {
+            console.log(valueOrPromise);
+          }
+        },
+        className: function(object) {
+          return object.__proto__.constructor;
+        },
+        registry: container.registry.dict,
+        routes: Em.keys(app.Router.router.recognizer.names),
+        store: container.lookup("store:main"),
+        typeMaps: container.lookup("store:main").typeMaps,
+        templates: Em.keys(Ember.TEMPLATES),
+        inspect: Em.inspect,
+        observersFor: function(observedObj, key){
+          Em.observersFor(observedObj, key);
+        }
+      };
+    }
+  });
+});
