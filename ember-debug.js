c77b5b5d4ae95aa323f80fa6486cc2c94eeb5c1e
Simon Wade <simon.wade@gmail.com>
2013-10-22 19:18:30 +1100

    Added more debug methods, improved logging, only add Ember Data methods if DS is defined

--- ember-debug.js
+++ ember-debug.js
@@ -1,6 +1,5 @@
-"use strict";
-
 Ember.onLoad('Ember.Application', function(Application) {
+  "use strict";
   return Application.initializer({
     name: 'debug',
     initialize: function(container, app) {
@@ -28,17 +27,44 @@ Ember.onLoad('Ember.Application', function(Application) {
           return object.__proto__.constructor;
         },
         registry: container.registry.dict,
-        routes: function() {
-          return Em.keys(app.Router.router.recognizer.names)
+        lookup: function(name) {
+          return container.lookup(name);
         },
-        store: container.lookup("store:main"),
-        typeMaps: container.lookup("store:main").typeMaps,
+        router: function(name) {
+          name = name || "main";
+          return container.lookup("router:" + name).get('router');
+        },
+        routes: function() {
+          return Em.keys(this.router().recognizer.names)
+        },
         templates: Em.keys(Ember.TEMPLATES),
         inspect: Em.inspect,
         observersFor: function(obj, property){
           Em.observersFor(obj, property);
+        },
+        logResolver: function(bool) {
+          bool = typeof bool == 'undefined' ? true : bool;
+          Em.ENV.LOG_MODULE_RESOLVER = bool;
+        },
+        logAll: function(bool) {
+          bool = typeof bool == 'undefined' ? true : bool;
+          app.LOG_ACTIVE_GENERATION = bool;
+          app.LOG_VIEW_LOOKUPS = bool;
+          app.LOG_TRANSITIONS = bool;
+          app.LOG_TRANSITIONS_INTERNAL = bool;
+        },
+        globalize: function() {
+          for( var prop in this ) {
+            if( this.hasOwnProperty(prop) ) {
+              window[prop] = window[prop] || this[prop];
+            }
+          }
         }
       };
+      if( DS ) {
+        app.debug.store = container.lookup("store:main");
+        app.debug.typeMaps = container.lookup("store:main").typeMaps;
+      }
     }
   });
 });
