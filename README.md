451f393c367d2f233981ad30e5bcf44a1cff8e03
Simon Wade <simon.wade@gmail.com>
2014-02-14 10:47:01 +1100

    Added a bunch of exciting new functions Changed to use Ember.Application.initializer() Several bug fixes

--- README.md
+++ README.md
@@ -15,19 +15,34 @@ When the code has been included in your app a `debug` object will be injected in
 
 ### `controller: function(name)`
 
-Returns the named controller.
+Returns the named controller. Defaults to the controller for the current route.
+
+### `model: function(name)`
+
+Returns the model for the named controller. Defaults to the controller for the current route.
 
 ### `route: function(name)`
 
-Returns the named route.
+Returns the named route. Defaults to the current route.
 
-### `view: function(id)`
+### `router: function(name)`
 
-Return the View instance with the specified `id` e.g. `ember352`.
+Returns the named router instance. Defaults to `router:main`.
+
+### `routes: function()`
+
+Returns the names of all routes.
+
+### `view: function(idOrDomElement)`
+
+Return the View instance with the specified id e.g. `ember352`. If an object 
+is provided (such as a DOM element) then the `id` property of the object will be 
+used.
 
 ### `log: function(valueOrPromise)`
 
-Logs the specified value, or waits until it's resolved if it's a `Promise`.
+Logs the specified value using `console.log`, or waits until it's resolved if 
+it has a `then` method (eg. it's a `Promise`).
 
 ### `className: function(object)`
 
@@ -35,11 +50,43 @@ Returns the class name of the object.
 
 ### `registry`
 
-Returns the injectable objects in the (poorly documented) container.
+Returns the hash of objects in the `container` registry.
 
-### `routes`
+### `lookup: function(name)`
 
-Returns the names of all routes.
+Performs a lookup for the named entry in the `container`, which will in turn
+ask its `resolver` if it's not found.
+
+### `containerNameFor: function(obj)`
+
+Searches the `container` registry to find the name for the specified object 
+(if any).
+
+### `templates`
+
+Returns the keys of all the `Ember.TEMPLATES`.
+
+### `inpsect`
+
+Does what it says, in a manner of speaking. Alias to `Ember.inspect`.
+
+### `observersFor: function(obj, property)`
+
+Returns the set of observers for the specified `property` on `obj`, an alias 
+to `Ember.observersFor`.
+
+### `logResolver: function(bool = true)`
+
+Switch logging for the resolver on/off.
+
+### `logAll: function(bool = true)`
+
+Switch logging for all the things on/off.
+
+### `globalize: function()`
+
+Attach all of these useful functions to the `window` object (eww!) - useful
+for accessing in the dev tools console.
 
 ### `store`
 
@@ -48,15 +95,3 @@ The Ember Data `Store`.
 ### `typeMaps`
 
 The Ember Data 'type map'.
-
-### `templates`
-
-Returns the names of all templates.
-
-### `inspect`
-
-Does what it says, in a manner of speaking.
-
-### `observersFor: function(obj, property)`
-
-Returns the set of observers for the specified `property` on `obj`.
--- ember-debug.js
+++ ember-debug.js
@@ -1,80 +1,94 @@
-Ember.onLoad('Ember.Application', function(Application) {
-  "use strict";
-  return Application.initializer({
-    name: 'debug',
-    initialize: function(container, app) {
-      app.debug = {
-        container: container,
-        controller: function(name) {
-          return container.lookup('controller:' + name);
-        },
-        route: function(name) {
-          return container.lookup('route:' + name);
-        },
-        view: function(domElem) {
-          return Em.View.views[domElem];
-        },
-        log: function(valueOrPromise) {
-          if (valueOrPromise.then) {
-            valueOrPromise.then(function(value) {
-              console.log(value);
-            });
-          }
-          else {
-            console.log(valueOrPromise);
-          }
-        },
-        className: function(object) {
-          return object.__proto__.constructor;
-        },
-        registry: container.registry.dict,
-        lookup: function(name) {
-          return container.lookup(name);
-        },
-        containerNameFor: function(object) {
-          var name;
-          container.cache.eachLocal(function(key, value) {
-            if (value == object) {
-              name = key;
-            }
+Ember.Application.initializer({
+  name: 'debug',
+  initialize: function(container, app) {
+    function alias(name) {
+      return function() {
+        debug[name].apply(null, arguments);
+      };
+    }
+    var debug = app.debug = {
+      container: container,
+      controller: function(name) {
+        name = name || debug.currentPath();
+        return container.lookup('controller:' + name);
+      },
+      model: function(name) {
+        var controller = debug.controller(name);
+        return controller && controller.get('model');
+      },
+      route: function(name) {
+        name = name || debug.currentPath();
+        return container.lookup('route:' + name);
+      },
+      router: function(name) {
+        name = name || 'main';
+        return container.lookup('router:' + name).get('router');
+      },
+      routes: function() {
+        return Em.keys(debug.router().recognizer.names);
+      },
+      view: function(id) {
+        if (typeof id === 'object') {
+          id = id.id;
+        }
+        return Em.View.views[id];
+      },
+      currentPath: function() {
+        return debug.controller('application').get('currentPath');
+      },
+      log: function(valueOrPromise) {
+        if (valueOrPromise.then) {
+          valueOrPromise.then(function(value) {
+            console.log(value);
           });
-          return name;
-        },
-        router: function(name) {
-          name = name || 'main';
-          return container.lookup('router:' + name).get('router');
-        },
-        routes: function() {
-          return Em.keys(this.router().recognizer.names)
-        },
-        templates: Em.keys(Ember.TEMPLATES),
-        inspect: Em.inspect,
-        observersFor: function(obj, property) {
-          Em.observersFor(obj, property);
-        },
-        logResolver: function(bool) {
-          bool = typeof bool == 'undefined' ? true : bool;
-          Em.ENV.LOG_MODULE_RESOLVER = bool;
-        },
-        logAll: function(bool) {
-          bool = typeof bool == 'undefined' ? true : bool;
-          app.LOG_ACTIVE_GENERATION = bool;
-          app.LOG_VIEW_LOOKUPS = bool;
-          app.LOG_TRANSITIONS = bool;
-          app.LOG_TRANSITIONS_INTERNAL = bool;
-        },
-        globalize: function() {
-          for (var prop in this) {
-            if (this.hasOwnProperty(prop)) {
-              window[prop] = window[prop] || this[prop];
-            }
+        }
+        else {
+          console.log(valueOrPromise);
+        }
+      },
+      className: function(object) {
+        return object.__proto__.constructor;
+      },
+      registry: container.registry.dict,
+      lookup: function(name) {
+        return container.lookup(name);
+      },
+      containerNameFor: function(object) {
+        var name;
+        container.cache.eachLocal(function(key, value) {
+          if (value === object) {
+            name = key;
+          }
+        });
+        return name;
+      },
+      templates: Em.keys(Ember.TEMPLATES),
+      inspect: Em.inspect,
+      observersFor: function(obj, property) {
+        Em.observersFor(obj, property);
+      },
+      logResolver: function(bool) {
+        bool = typeof bool == 'undefined' ? true : bool;
+        Em.ENV.LOG_MODULE_RESOLVER = bool;
+      },
+      logAll: function(bool) {
+        bool = typeof bool == 'undefined' ? true : bool;
+        app.LOG_ACTIVE_GENERATION = bool;
+        app.LOG_VIEW_LOOKUPS = bool;
+        app.LOG_TRANSITIONS = bool;
+        app.LOG_TRANSITIONS_INTERNAL = bool;
+      },
+      globalize: function() {
+        for (var prop in this) {
+          if (this.hasOwnProperty(prop)) {
+            window[prop] = window[prop] || this[prop];
           }
         }
-      };
-      if (typeof DS == 'object') {
-        app.debug.store = container.lookup('store:main');
-        app.debug.typeMaps = container.lookup('store:main').typeMaps;
       }
+    };
+    if (typeof DS == 'object') {
+      app.debug.store = container.lookup('store:main');
+      app.debug.typeMaps = container.lookup('store:main').typeMaps;
     }
-  });
+  }
 });
