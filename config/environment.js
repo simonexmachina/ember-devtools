1cc799659033c663672aabbf91a6fd70c76052d3
Simon Wade <simon.wade@gmail.com>
2016-02-10 22:27:25 +1100

    Re-add object-assign module

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,12 @@
 /* jshint node:true */
+var objectAssign = require('object-assign');
 
 /**
  * Export the default config for ember-devtools. By default, enable only
  * in development.
  */
 module.exports = function(environment, appConfig) {
-  appConfig['ember-devtools'] = Object.assign({
+  appConfig['ember-devtools'] = objectAssign({
     enabled: environment === 'development',
     global: false
   }, appConfig['ember-devtools'] || {});
